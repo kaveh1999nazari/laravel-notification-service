faaa24150195a88905c2ffd5a5a225dade138534
kaveh <kaveh1999nazari@gmail.com>
2025-02-17 08:07:53 +0330

    first commit

--- src/NotificationServiceProvider.php
+++ src/NotificationServiceProvider.php
@@ -18,10 +18,8 @@ class NotificationServiceProvider extends ServiceProvider
                 GenerateNotification::class,
             ]);
 
-            $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-
             $this->publishes([
-                __DIR__.'/../database/migrations/' => database_path('migrations'),
+                __DIR__.'/Migrations/' => database_path('migrations'),
             ], 'migrations');
 
             $this->publishes([
